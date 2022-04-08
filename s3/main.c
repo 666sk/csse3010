dbb7a9c1aa3915d1270877ed053776774c16f82e
Kuang student <s4575272@student.uq.edu.au>
2022-04-09 02:26:34 +1000

    DT2 Not finished

--- s3/main.c
+++ s3/main.c
@@ -1,8 +1,6 @@
-
 #include "board.h"
 #include "processor_hal.h"
 #include "debug_log.h"
-
 
 #define S0  0  //IDLE State
 #define S1  1  //Encode State
@@ -15,7 +13,7 @@ int fsm_processing(int current, char CH1, char CH2);
 unsigned char s4575272_lib_hamming_hbyte_encode(unsigned char in);
 unsigned short s4575272_lib_hamming_byte_encode(unsigned char input);
 unsigned char s4575272_lib_hamming_byte_decode(unsigned char value);
-
+int pb;
 
 int main(void) {
 
@@ -23,9 +21,7 @@ int main(void) {
     BRD_debuguart_init();
     s4575272_reg_lta1000g_init(); //Initialise GPIO pins for LED 
     s4575272_reg_joystick_pb_init(); //Initialise GPIO pins for joystick
-
 	BRD_LEDInit();		//Initialise LEDS
-
 	// Turn off LEDs
 	BRD_LEDGreenOff();
 
@@ -34,16 +30,18 @@ int main(void) {
     int currentState = S0;
     char recvChar1;
     char recvChar2;
+    char a,b;
     int collector = 0;
     int flag = 0;
+    int p = 0;
     while(1) {
-        
+        pb = s4575272_reg_joystick_press_get() % 2;
         if ((HAL_GetTick() - prev_tick) >= 300) {
             prev_tick =  HAL_GetTick();
             if (collector == 0) {
                 if ((recvChar1 = BRD_debuguart_getc()) != '\0') {
                     collector = 1;
-                    
+                    a = recvChar1;
                     debug_log("Character1: %c - ASCII Table value: %d\n\r", recvChar1, recvChar1);
                 }
             }
@@ -51,6 +49,8 @@ int main(void) {
             if (collector == 1) {
                 if ((recvChar2 = BRD_debuguart_getc()) != '\0') {
                     collector = 0;
+                    p = 0;
+                    b = recvChar2;
                     debug_log("Character2: %c - ASCII Table value: %d\n\r", recvChar2, recvChar2);
                 }
             }
@@ -58,6 +58,53 @@ int main(void) {
             if ((recvChar1 != '\0') && (recvChar2 != '\0')) {
                 currentState = fsm_processing(currentState, recvChar1, recvChar2);
                 //debug_log("Sys time %d\n\r", HAL_GetTick());
+            }
+            if ((currentState == S1) && (collector == 0) && (a!='E') && (b != 'E')) {
+                uint8_t uncodedWord1 = 0;
+                uint8_t uncodedWord2 = 0;
+                uint8_t codedWord1;
+                uint8_t codedWord2;
+               
+                if (a >= '1' && a <= '9') {  //ALWAYS BE TRUE. ITS A PROBLEM
+                    uncodedWord1 |= ((a - 48) << 0);
+                } else if (a >= 'A' && a <= 'F') {
+                    uncodedWord1 |= ((a - 55) << 0);
+                }
+
+                if (a >= '1' && a <= '9') {
+                    uncodedWord2 |= ((b - 48) << 0);
+                } else if (a >= 'A' && a <= 'F') {
+                    uncodedWord2 |= ((b - 55) << 0);
+                }
+                
+                if (p == 0){
+                    debug_log("uncodedWOrd1 %d\n\r", uncodedWord1);
+                    debug_log("uncodedWOrd2 %d\n\r", uncodedWord2);
+                    codedWord1 = s4575272_lib_hamming_hbyte_encode(uncodedWord1);
+                    codedWord2 = s4575272_lib_hamming_hbyte_encode(uncodedWord2);
+                    debug_log("COdedWOrd1 %d\n\r", codedWord1);
+                    debug_log("COdedWOrd2 %d\n\r", codedWord2);
+                    p = 1;
+                }
+
+                if (pb == 0) {
+                    for (int i = 0; i < 8; i++) {
+                        if (codedWord1 & (1 << i)) {
+                            lta1000g_seg_set(i, '1');
+                        } else {
+                            lta1000g_seg_set(i, '0');
+                        }
+                    }
+                } else if (pb == 1) {
+                    for (int i = 0; i < 8; i++) {
+                        if (codedWord2 & (1 << i)) {
+                            lta1000g_seg_set(i, '1');
+                        } else {
+                            lta1000g_seg_set(i, '0');
+                        }
+                    }
+                }
+                
             }    
         }
     }
@@ -89,46 +136,6 @@ int fsm_processing(int current, char CH1, char CH2) {
                 nextState = S2;
             } else {
                 nextState = S1;
-                BRD_LEDGreenOn();
-                uint8_t uncodedWord1 = 0;
-                uint8_t uncodedWord2 = 0;
-                uint8_t codedWord1;
-                uint8_t codedWord2;
-                
-                //uint16_t codedWord;
-                if (47 < CH1 < 58) {
-                    uncodedWord1 |= ((CH1-48) << 0);
-                } else if (64 < CH1 < 71) {
-                    uncodedWord1 |= ((CH1-55) << 0);
-                } else {
-                    return S1;
-                }
-                if (47 < CH2 < 58) {
-                    uncodedWord2 |= ((CH2-48) << 0);
-                } else if (64 < CH2 < 71) {
-                    uncodedWord2 |= ((CH2-55) << 0);
-                } else {
-                    return S1;
-                }
-                //encode here?
-                
-                    debug_log("uncodedWOrd1 %d\n\r", uncodedWord1);
-                    debug_log("uncodedWOrd2 %d\n\r", uncodedWord2);
-                    codedWord1 = s4575272_lib_hamming_hbyte_encode(uncodedWord1);
-                    codedWord2 = s4575272_lib_hamming_hbyte_encode(uncodedWord2);
-                    debug_log("COdedWOrd1 %d\n\r", codedWord1);
-                    debug_log("COdedWOrd2 %d\n\r", codedWord2);
-                
-
-                for (int i = 0; i < 8; i++) {
-                    if (codedWord1 & (1 << i)) {
-                        lta1000g_seg_set(i, '1');
-                    } else {
-                        lta1000g_seg_set(i, '0');
-                    }
-                }
-             
-                
             }
             break;
 
@@ -139,28 +146,6 @@ int fsm_processing(int current, char CH1, char CH2) {
                 nextState = S1;
             } else {
                 nextState = S2;
-                BRD_LEDBlueOn();
-                uint8_t codedWord;
-                uint8_t decodedWord;
-                if (47 < CH1 < 58) {
-                    codedWord |= ((CH1-48) << 4);
-                } else if (64 < CH1 < 71) {
-                    codedWord |= ((CH1-55) << 4);
-                } else {
-                    return S2;
-                }
-                if (47 < CH2 < 58) {
-                    codedWord |= ((CH2-48) << 0);
-                } else if (64 < CH2 < 71) {
-                    codedWord |= ((CH2-55) << 0);
-                } else {
-                    return S2;
-                }
-                debug_log("codedWOrd %d\n\r", codedWord);
-                decodedWord = s4575272_lib_hamming_byte_decode(codedWord);
-                debug_log("decodedWord %d\n\r", decodedWord);
-
-
             }
             break;
     }
